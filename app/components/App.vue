42e9ebfa4a6cc42babebf2d9ff8c29ef18f155ad
Tiago Alves <tralves@gmail.com>
2019-04-09 12:41:56 +0100

    Initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <Page>
+    <ActionBar title="Welcome to NativeScript-Vue!"/>
+    <StackLayout>
+      <Label class="message" text="OLD SYNTAX (slot-scope)" col="0" row="0"/>
+      <CurrentUser>
+        <template slot-scope="scope">{{scope.user.firstName}} {{scope.user.lastName}}</template>
+      </CurrentUser>
+
+      <Label class="message" text="NEW SYNTAX (v-slot)" col="0" row="0"/>
+      <CurrentUser>
+        <template v-slot="scope">{{ scope ? scope.user.firstName : '(scope is not defined)'}}</template>
+      </CurrentUser>
+    </StackLayout>
+  </Page>
+</template>
+
+<script >
+import CurrentUser from "./CurrentUser";
+
+export default {
+  components: { CurrentUser },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+  margin-top: 50;
+}
+</style>
---
+++ app/components/CurrentUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <Label>
+    <slot :user="user">{{ user.firstName }}</slot>
+  </Label>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        firstName: "Tiago",
+        lastName: "Alves"
+      }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
